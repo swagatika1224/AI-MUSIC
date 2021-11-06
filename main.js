f24713a6d0c76c82025f91d231c53c0d451ddd3e
swagatika1224 <81203604+swagatika1224@users.noreply.github.com>
2021-11-05 19:03:09 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 song1 = "";
-
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload() {
 song1 = loadSound("music.mp3");
@@ -13,6 +16,9 @@ canvas.center();
 
 video = createCapture(VIDEO);
 video.hide();
+
+posenet = ml5.poseNet(video,modeLoaded);
+posenet.on('pose', gotPoses);
 }
 
 function draw() {
@@ -20,5 +26,23 @@ image(video, 0, 0, 600, 500);
 }
 
 function play() {
-song.play();
+song1.play();
+song2.play();
+}
+
+function modeLoaded() {
+console.log('PoseNet is Initialized');
+}
+
+function gotPoses(results) {
+if(results.length > 0) {
+console.log(results);
+leftWristX = results[0].pose.leftWrist.x;
+leftWristY = results[0].pose.leftWrist.y;
+console.log("leftWristX = " + leftWristX +"leftWristY = "+ leftWristY)
+
+rightWristX = results[0].pose.rightWrist.x;
+rightWristY = results[0].pose.rightWrist.y;
+console.log("rightWristX = " + rightWristX +"rightWristY = "+ rightWristY)
+  }
 }
